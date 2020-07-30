9f692d5f50a8f8dff6b8cee6b99c0f99da172c06
matthewSkipworth <mattskipworth@gmail.com>
2020-07-30 08:09:35 -0700

    from mac

--- Section 9/Challenge - Create a Tic Tac Toe Game/main.c
+++ Section 9/Challenge - Create a Tic Tac Toe Game/main.c
@@ -7,9 +7,9 @@ int main(void)
 
     char (*boardptr)[];
 
-    char tiles[] = {' ',' ',' ',
-                    ' ',' ',' ',
-                    ' ',' ',' '};
+    char tiles[] = {'1','2','3',
+                    '4','5','6',
+                    '7','8','9'};
 
     boardptr = &tiles;
 
